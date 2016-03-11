b605cd8849b736d67030ae77a73e70da8bdf7236
Stephano <amunra@unm.edu>
2016-03-10 22:44:56 -0700

    Adds src folder with initial two packages (cs351 being the main package)

---
+++ src/cs351/project2/Placeholder.java
@@ -0,0 +1,12 @@
+package cs351.project2;
+
+/**
+ * Placeholder class
+ */
+public class Placeholder
+{
+  public static void main(String[] args)
+  {
+    throw new RuntimeException("I'm not a real class");
+  }
+}
