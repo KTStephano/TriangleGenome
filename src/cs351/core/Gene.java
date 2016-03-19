a624ef09c00e48d50a4489f2b1cb76341b44bde8
Stephano <amunra@unm.edu>
2016-03-18 23:33:24 -0600

    Adds some extra functionality to the Gene class

--- src/cs351/core/Gene.java
+++ src/cs351/core/Gene.java
@@ -7,17 +7,22 @@ package cs351.core;
 public class Gene
 {
   private int value;
+  private final int MIN_VALUE, MAX_VALUE;
   private GeneTypes type;
 
   /**
    * Constructs a new Gene with the given value/type.
    *
    * @param value value for the gene
+   * @param minValue the minimum value that this gene can take on
+   * @param maxValue the maximum value that this gene can take on
    * @param type GeneTypes object
    */
-  public Gene(int value, GeneTypes type)
+  public Gene(int value, int minValue, int maxValue, GeneTypes type)
   {
     this.value = value;
+    MIN_VALUE = minValue;
+    MAX_VALUE = maxValue;
     this.type = type;
   }
 
@@ -29,6 +34,26 @@ public class Gene
   public int getValue()
   {
     return value;
+  }
+
+  /**
+   * Gets the minimum value that this gene can take on.
+   *
+   * @return min value
+   */
+  public int getMinValue()
+  {
+    return MIN_VALUE;
+  }
+
+  /**
+   * Gets the maximum value that this gene can take on.
+   *
+   * @return max value
+   */
+  public int getMaxValue()
+  {
+    return MAX_VALUE;
   }
 
   /**
@@ -49,6 +74,9 @@ public class Gene
    */
   public void mutate(int newVal)
   {
+    // Basic bounds checking
+    if (newVal < MIN_VALUE) newVal = MIN_VALUE;
+    else if (newVal > MAX_VALUE) newVal = MAX_VALUE;
     value = newVal;
   }
 }
