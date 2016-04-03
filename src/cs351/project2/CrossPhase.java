9f395bd433da5c1390a7f526830dd6576dba05fe
Stephano <amunra@unm.edu>
2016-04-02 23:51:45 -0600

    Adds CrossPhase - We can edit this to test

--- src/cs351/project2/CrossPhase.java
+++ src/cs351/project2/CrossPhase.java
@@ -1,14 +1,18 @@
 package cs351.project2;
 
+import cs351.core.Cross;
+
 /**
  * TODO this class is pretty bad
  */
 public class CrossPhase
 {
   private final Engine ENGINE;
+  private final Cross CROSS;
 
   public CrossPhase(Engine engine)
   {
     ENGINE = engine;
+    CROSS = new GameCross();
   }
 }
